445f1b59c67303d733fb94ea44c19175dbfd22f4
Pinetea <121543559+Pinetea@users.noreply.github.com>
2022-12-29 15:44:48 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,13 @@ importimport { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+
+
+export default defineConfig({
+  site: 'https://pinetea.github.io',
+  base: '/',
+})
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
